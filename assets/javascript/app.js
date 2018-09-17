509d1d3f046edd85d13675d6c923d535a89975a0
riffon3000 <riffon@gmail.com>
2018-09-16 22:18:23 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,55 +5,63 @@ $(document).ready(function () {
             question: "What was the first full length CGI movie?",
             choices: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
             answer: "Toy Story",
-            reference: "toy",
+            name: "toy",
+            class:".toy",
         },
         {
             question: "Which of these is NOT a name of one of the Spice Girls?",
             choices: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
             answer: "Fred Spice",
-            reference: "fred",
+            name: "fred",
+            class:".fred",
         },
         {
             question: "Which NBA team won the most titles in the 90's?",
             choices: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
             answer: "Chicago Bulls",
-            reference: "bulls",
+            name: "bulls",
+            class:".bulls",
         },
         {
             question: "Which group released the hit song, \"Smells Like Teen Spirit\"?",
             choices: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
             answer: "Nirvana",
-            reference: "nirvana",
+            name: "nirvana",
+            class:".nirvana",
         },
         {
             question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
             choices: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
             answer: "The Lion King",
-            reference: "king",
+            name: "king",
+            class:".king",
         },
         {
             question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
             choices: ["Dice", "Mirror", "Fresh", "Cab"],
             answer: "Fresh",
-            reference: "fresh",
+            name: "fresh",
+            class:".fresh",
         },
         {
             question: "What was Doug's best friend's name?",
             choices: ["Skeeter", "Mark", "Zach", "Cody"],
             answer: "Skeeter",
-            reference: "skeeter",
+            name: "skeeter",
+            class:".skeeter",
         },
         {
             question: "What was the name of the principal at Bayside High in Saved By The Bell?",
             choices: ["Mr. Zhou", "Mr. Driggers", "Mr. Belding", "Mr. Page"],
             answer: "Mr. Belding",
-            reference: "belding",
+            name: "belding",
+            class:".belding",
         },
     ]
 
-    var start = $("#start-btn").on("click", function () {
-        $("#start").hide();
-        $("#trivia").show();
+    var start = $('#start-btn').on('click', function () {
+        $('#start').hide();
+        $('#trivia').show();
         counter(30);
         displayQuestions();
     });
@@ -63,7 +71,7 @@ $(document).ready(function () {
 
         var timeRemaining = setInterval(function () {
             sec = sec--;
-            $("#time-remaining").text("Time remaining: " + sec + " seconds");
+            $('#time-remaining').html('Time remaining: ' + sec + ' seconds');
 
             if (sec <= 0) {
                 triviaResult();
@@ -72,22 +80,26 @@ $(document).ready(function () {
         }, 1000);
 
         // click event for submit button that stops timer
-        $("#submit-btn").on("click", function () {
+        $('#submit-btn').on('click', function () {
             clearInterval(timeRemaining);
         })
     };
 
+    var labels =["1st", "2nd", "3rd","4th"];
+
     var displayQuestions = function () {
-        $("#questions :not(\"#submit-btn\")").empty();
+        $('#questions :not("#submit-btn")').empty();
 
         // loops through questions
         for (var j = 0; j < questions.length; j++) {
-            $("#questions").append("<div>" + questions[j].question + "</div>");
+            $('#questions').prepend('div class="'+questions[j].name+'"></div>');
+            $(questions[j].class).append('<div>'+questions[j].question+'</div>');
         }
         //   loops through choices
-        for (var z = 0; z <= questions[z].choices.length; z++) {
-            $("#questions").append("<input type=\"radio\" name=\"" + questions[z].reference + "value=\"" + questions[z].answer)
+        for (var i = 0; i < questions.choices.length; i++) {
+            $(questions[j].class).append('<input type="radio" name="' + questions[j].name + '"value="' + questions[j].choices[i] + '"/><label for="' + labels[i] + '">' + questions[j].choices[i] + '</label>');
         }
+        $('#questions').prepend('<hr></hr>');
     }
 
     var triviaResult = function() {
@@ -97,8 +109,8 @@ $(document).ready(function () {
         var totalUnanswered = questions.length - (totalCorrect + totalWrong);
 
         // loop through questions array to check answers
-        for (var i = 0; i < questions.lenth; i++) {
-            if ($("input:radio[name=\"" + questions[i].reference + "\"]:checked").val() === questions[i].answer) {
+        for (var z = 0; z < questions.lenth; z++) {
+            if ($("input:radio[name=\"" + questions[z].name + "\"]:checked").val() === questions[z].answer) {
                 totalCorrect++;
             }
             else {
